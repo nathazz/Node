510d78df4f1b0018945f1fbf4052cdad630e6f1d
Nathan silva <105741461+nathazz@users.noreply.github.com>
2024-03-22 23:33:57 -0300

    proj #1

--- TEMPLATE_ENGINE/Tarefa/app.js
+++ TEMPLATE_ENGINE/Tarefa/app.js
@@ -1,8 +1,6 @@
-const handlebars = require('express-handlebars')
 const express = require('express')
-
-const app = require("./server.js")
-
+const handlebars = require('express-handlebars')
+const app = express()
 
 
 app.engine('handlebars', handlebars.engine())
@@ -10,22 +8,40 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 
+//mexer com form
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended:false }))
 
 app.get('/', (req, res) =>{
+    res.render('home')
+})
+
+app.get('/itens',(req,res) =>{
     const produtos = {
         chair_one:"Slim black chair",
         chair_two:"Bar stool",
         vase:"Vase white"
     }
 
-    res.render('home', {produtos})
+    res.render('itens', {produtos} )
 })
 
 
+app.get('/form', (req, res) =>{
+    res.status(200).render('form')
+
+})
+
+app.post('/cadastro', (req,res) =>{
+    const body = req.body
+    console.log(body);
+    res.end()
+})
+
 
 const PORT = 3000
-
-
 app.listen(PORT, () =>{
     console.log(`Servidor rodando na porta http://localhost:${PORT}`)
 })
